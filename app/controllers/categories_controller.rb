1e24b7f82e69c50014048315d933366f2e7c3c26
SarahNox <apnoeist@googlemail.com>
2016-06-23 09:32:47 +0200

    diplaying products by category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,48 @@
 class CategoriesController < ApplicationController
   def index
-    @categories = Category.find_by(name: params[:category])
-    @products = Product.where(category: @category).order("name ASC")
+    @categories = Category.all
   end
 
   def show
     @category = Category.find(params[:id])
+    @products = Product.where(:category_id => params[:id])
   end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update_attributes(category_params)
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    @category.delete
+    redirect_to root_path
+  end
+  
+  private
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
 end
