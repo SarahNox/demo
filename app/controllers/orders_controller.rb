fd3ee3ad03432f549d741c2792a76ad023d6e376
SarahNox <apnoeist@googlemail.com>
2016-06-26 19:30:38 +0200

    display orders by userID

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,17 +1,21 @@
 class OrdersController < ApplicationController
   def index
-    @orders = Order.all
-    # @order = Order.find(params[:id])
+    @orders = current_user.orders
   end
 
   def create 
-    @order = Order.new
-    session[:cart].each do |product_id, quantity|
-    @order.order_items.build(product_id: product_id, quantity: quantity)
-    end
-    @order.save
-    session[:cart] = nil
-    redirect_to order_path(:id => @order.id)
+    # if session[:cart].empty?
+    #   redirect_to root_path
+    # else
+      @order = Order.new
+      @order.user = current_user
+      session[:cart].each do |product_id, quantity|
+        @order.order_items.build(product_id: product_id, quantity: quantity)
+      end
+      @order.save
+      session[:cart] = nil
+      redirect_to order_path(:id => @order.id)
+    # end
   end
 
   def show
