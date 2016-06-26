eb63efb297ff737e7212e2670fb85256a2d0a6ac
SarahNox <apnoeist@googlemail.com>
2016-06-26 14:30:45 +0200

    create Order and clean Cart

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,12 +5,16 @@ class OrdersController < ApplicationController
   end
 
   def create 
+    @order = Order.new
     session[:cart].each do |product_id, quantity|
-      OrderItem.create(product_id: product_id, quantity: quantity)
+    @order.order_items.build(product_id: product_id, quantity: quantity)
     end
+    @order.save
+    session[:cart] = nil
+    redirect_to order_path(:id => @order.id)
   end
 
   def show
-    @orders = Order.find(params[:id])
+    @order = Order.find(params[:id])
   end
 end
